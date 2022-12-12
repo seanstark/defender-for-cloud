43f1f2df46c40f0df1ebbdbee5a95c62a2a8b0a4
seanstark-ms <84108246+seanstark@users.noreply.github.com>
2022-12-12 14:56:54 -0800

    Create get-integration-report.ps1

---
+++ get-integration-report/get-integration-report.ps1
@@ -0,0 +1,36 @@
+#Get All Subscriptions
+$subscriptions = Get-AzSubscription -TenantId (Get-AzContext).Tenant | Where State -eq 'Enabled'
+
+$friendlysettings = @()
+
+ForEach ($subscription in $subscriptions){
+    $settings = $null
+    $settings = ((Invoke-AzRestMethod -SubscriptionId $subscription.Id -ResourceProviderName 'Microsoft.Security' -ResourceType 'settings' -ApiVersion '2022-05-01' -Method Get).Content | ConvertFrom-Json).Value
+    $defenderForServersPlan = (Invoke-AzRestMethod -SubscriptionId $subscription.Id -ResourceProviderName 'Microsoft.Security' -ResourceType 'pricings' -Name 'VirtualMachines' -ApiVersion '2022-03-01' -Method Get).Content | ConvertFrom-Json
+    $subscription.Name
+    if($settings){
+        $friendlysettings += ([PSCustomObject]@{
+            subscriptionName = $subscription.Name
+            subscriptionId = $subscription.Id
+            DefenderforServersPlan = $(if($defenderForServersPlan.properties.subPlan -eq $null){'notenabled'}else{$defenderForServersPlan.properties.subPlan})
+            DefenderforCloudApps = ($settings | where name -eq 'MCAS').Properties.enabled
+            DefenderforEndpoint = ($settings | where name -eq 'WDATP').Properties.enabled
+            DefenderforEndpointLinux = ($settings | where name -eq 'WDATP_EXCLUDE_LINUX_PUBLIC_PREVIEW').Properties.enabled
+            DefenderforEndpointUnifiedAgent = ($settings | where name -eq 'WDATP_UNIFIED_SOLUTION').Properties.enabled
+            SentinelBiDirectionalAlertSync = ($settings | where name -eq 'Sentinel').Properties.enabled
+            error = $null
+        })
+    }else{
+        $friendlysettings += ([PSCustomObject]@{
+            subscriptionName = $subscription.Name
+            subscriptionId = $subscription.Id
+            DefenderforServersPlan = 'no settings returned'
+            DefenderforCloudApps = 'no settings returned'
+            DefenderforEndpoint = 'no settings returned'
+            DefenderforEndpointLinux = 'no settings returned'
+            DefenderforEndpointUnifiedAgent = 'no settings returned'
+            SentinelBiDirectionalAlertSync = 'no settings returned'
+            error = ('No Settings Returned for Subscription: {0}, you may not have security reader rights assigned' -f $subscription.Name)
+        })
+    }
+}
